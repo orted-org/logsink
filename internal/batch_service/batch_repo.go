189015999ddebfd1bf5fa2deb7344aa7d1a66629
himanshu-sah <itsmehs07@gmail.com>
2022-01-04 16:02:17 +0545

    batch repo for storing data in batches

---
+++ internal/batch_service/batch_repo.go
@@ -0,0 +1,26 @@
+package batchservice
+
+type BatchRepo struct {
+	maxSize int
+	index   int
+	items   []interface{}
+}
+
+func NewBatchRepo(maxSize int) *BatchRepo {
+	return &BatchRepo{
+		maxSize: maxSize,
+		index:   -1,
+		items:   make([]interface{}, maxSize),
+	}
+}
+
+func (b *BatchRepo) Push(item interface{}) {
+	if b.index < b.maxSize {
+		b.index++
+		b.items[b.index] = item
+	}
+}
+
+func (b *BatchRepo) Size() int {
+	return b.index + 1
+}
