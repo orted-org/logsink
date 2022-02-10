086cb61e3afb870f27ff93b890bd3447cff4d6cb
himanshu-sah <itsmehs07@gmail.com>
2022-02-11 00:56:38 +0545

    store init for service

---
+++ frontend/src/Logic/Stores/SService.ts
@@ -0,0 +1,27 @@
+import { observable, action, runInAction, makeAutoObservable } from "mobx";
+import MService from "../Model/MService";
+import { RService } from "../Repository/RService";
+
+export class SService {
+  @observable serviceList: MService[] = [];
+  @observable status: string | null = null;
+  constructor(private serviceRepo: RService) {
+    makeAutoObservable(this);
+  }
+
+  @action
+  fetchServiceList = async () => {
+    this.status = "Fetching services";
+    try {
+      const serviceList = await this.serviceRepo.getServiceList();
+      runInAction(() => {
+        this.serviceList = serviceList;
+        this.status = null;
+      });
+    } catch (err: any) {
+      runInAction(() => {
+        this.status = err.message || "Failed to fetch services";
+      });
+    }
+  };
+}
