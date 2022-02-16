c014bcecb5f7df8352f8e630ffdd357c0b70a8ca
himanshu-sah <itsmehs07@gmail.com>
2022-02-16 14:04:12 +0545

    added routes to serve react app as static files app

--- cmd/http-server/handler.go
+++ cmd/http-server/handler.go
@@ -8,17 +8,25 @@ import (
 
 func initHandler(app *App, r *chi.Mux) {
 
-	// auth
-	r.Post("/auth", app.handleLogin)
-	r.Delete("/auth", app.handleCheckAllowance(http.HandlerFunc(app.handleLogout)))
+	r.Route("/api", func(r chi.Router) {
+		// auth
+		r.Post("/auth", app.handleLogin)
+		r.Delete("/auth", app.handleCheckAllowance(http.HandlerFunc(app.handleLogout)))
 
-	// service management
-	r.Get("/service", app.handleCheckAllowance(http.HandlerFunc(app.handleGetService)))
-	r.Post("/service", app.handleCheckAllowance(http.HandlerFunc(app.handleAddService)))
-	r.Put("/service/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleUpdateService)))
-	r.Delete("/service/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleDeleteService)))
+		// service management
+		r.Get("/service", app.handleCheckAllowance(http.HandlerFunc(app.handleGetService)))
+		r.Post("/service", app.handleCheckAllowance(http.HandlerFunc(app.handleAddService)))
+		r.Put("/service/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleUpdateService)))
+		r.Delete("/service/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleDeleteService)))
+	})
 
 	// sink
 	r.Get("/sink", app.handleSink)
 
+	// WebUI
+	r.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "text/html; charset=utf-8")
+		http.ServeFile(rw, r, "./build/index.html")
+	})
+	r.Handle("/static/*", http.FileServer(http.Dir("./build")))
 }
